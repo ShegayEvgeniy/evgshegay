093666ad47cd9710e526b89dc8379eee2dd5c05d
Shegay Evgeniy <job4jstudy2@gmail.com>
2017-12-15 10:03:42 +0900

    1.1 Создание Hello World [141]

---
+++ src/ru/job4j/Calculate.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+
+/**
+ * Calculate class.
+ *@author Evgeniy Shegay (terrasan30@gmail.com)
+ *since 15.11.2017
+ */
+
+
+public class Calculate {
+
+   public static void main(String[] args) {
+       System.out.println("Hello World.");
+   }
+}
