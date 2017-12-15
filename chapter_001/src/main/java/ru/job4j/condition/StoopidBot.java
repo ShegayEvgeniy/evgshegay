db1a4e239fbe1031378e6a818f5c23c10d306c7c
Shegay Evgeniy <job4jstudy2@gmail.com>
2017-12-15 11:53:44 +0900

    3.1 Глупый бот [31323]

---
+++ chapter_001/src/main/java/ru/job4j/condition/StoopidBot.java
@@ -0,0 +1,36 @@
+package ru.job4j.condition;
+
+/**
+ * @author Evgeniy Shegay (terrasan30@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class StoopidBot {
+
+
+	String a = "Привет, Бот.";
+	String b = "Привет, умник.";
+	String c = "Пока.";
+	String d = "До скорой встречи.";
+	String e = "Сколько будет 2 + 2?";
+	String h = "Это ставит меня в тупик. Спросите другой вопрос.";
+	String g;
+
+	
+	public String answer(String question) {
+
+		if (question == a) {
+			g = b;
+		}
+		if  (question == c) {
+			g = d;
+		}
+		if (question == e) {
+			g = h;
+		}
+		return g;
+
+	}
+
+
+}
